34b50ade2695461499b40f74a5ba79db25c05551
Antonella Schiavoni <antonellaschiavoni2@gmail.com>
2017-09-05 18:22:09 -0300

    Initial commit

---
+++ app/src/main/java/guessme/ppc2017/unnoba/edu/ar/gametwo/MainActivity.java
@@ -0,0 +1,13 @@
+package guessme.ppc2017.unnoba.edu.ar.gametwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
